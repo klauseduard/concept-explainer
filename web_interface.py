1407a6b6a5dca28ce4b2ec13517a416074b1919e
Klaus-Eduard Runnel <klaus-eduard.runnel@nortal.com>
2023-07-20 12:35:56 +0300

    Get port from environment if possible

--- web_interface.py
+++ web_interface.py
@@ -125,5 +125,6 @@ def search_semantic():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
