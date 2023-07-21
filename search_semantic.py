abaa22ba08bb287de513b1155823c8076f9955e7
Klaus-Eduard Runnel <klaus-eduard.runnel@nortal.com>
2023-07-22 00:53:31 +0300

    Title: Fix for handling tensor to numpy conversions and reshaping embeddings
    Description:
    1. Modified the load_embeddings_and_hashes function to correctly load numpy files.
    2. Updated the get_semantic_scores function to convert tensor to numpy before applying cosine_similarity.
    3. Ensured embeddings are reshaped into the correct dimensions before passing to cosine_similarity function.
    4. Converted the indices dictionary values to integers during loading to ensure correct indexing of embeddings.

--- search_semantic.py
+++ search_semantic.py
@@ -53,7 +53,7 @@ def load_embeddings_and_hashes(embeddings_file, hashes_file):
     embeddings = None
 
     try:
-        embeddings = np.load(embeddings_file, allow_pickle=True) 
+        embeddings = np.load(embeddings_file, allow_pickle=True)
         # 'allow_pickle=True' is added just in case the numpy arrays are not able to be loaded due to the numpy version
     except FileNotFoundError:
         print(f"Embeddings file not found: {embeddings_file}")
@@ -99,10 +99,10 @@ def build_and_save_embeddings(directory, model_name, embeddings_path, indices_pa
 
 
 def get_semantic_scores(query, model, embeddings, filenames, indices):
-    query_vec = model.encode([query])[0]
+    query_vec = model.encode([query], convert_to_tensor=True)[0].cpu().numpy()  # Convert tensor to numpy
     scores = []
     for filename in filenames:
-        score = cosine_similarity([query_vec], [embeddings[indices[filename]]])[0][0]
+        score = cosine_similarity(query_vec.reshape(1, -1), embeddings[indices[filename]].reshape(1, -1))[0][0] 
         scores.append((filename, score))
     scores.sort(key=lambda x: x[1], reverse=True)
     return scores[:5]
@@ -124,7 +124,7 @@ if __name__ == "__main__":
 
     # Load the indices dictionary
     with open(indices_path, 'r') as f:
-        indices = json.load(f)
+        indices = {k: int(v) for k, v in json.load(f).items()}
 
     # Check if a query was passed as command-line argument
     if len(sys.argv) > 1:
